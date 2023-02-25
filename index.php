a6c98820571452a63b9a451041162be402aa75ab
fahmief <fahmiefendi644@gmail.com>
2023-02-25 21:24:55 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,11 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt placeat deleniti tempore iusto rem nam saepe praesentium! Explicabo sapiente dolorem, aut ut voluptate cumque enim voluptates blanditiis repudiandae consectetur iste?</p>
+
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 </html>
